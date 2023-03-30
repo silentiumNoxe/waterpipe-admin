dce5c836b1ac847696ce8992eaeadb86e0dab4d5
silentiumnoxe <silentium.noxe@gmail.com>
2023-03-30 22:29:28 +0300

    node page

---
+++ page/node.js
@@ -0,0 +1,147 @@
+Konva.DEFAULT_FONT = "Ubuntu";
+Konva.Color = {
+    PRIMARY: "#212525",
+    LIGHT: "#a0a8a8",
+
+    SUCCESS: "#4d7c45",
+    WARNING: "#a24444",
+    ERROR: "#bb853e"
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    showConnectedServer()
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const stage = new Konva.Stage({container: "editor", width, height, draggable: true});
+
+    window.mousePosition = function () {
+        return stage.getPointerPosition();
+    }
+
+    stage.on("wheel", e => {
+        const scaleBy = 1.05;
+        // stop default scrolling
+        e.evt.preventDefault();
+
+        const oldScale = stage.scaleX();
+        const pointer = stage.getPointerPosition();
+
+        const mousePointTo = {
+            x: (pointer.x - stage.x()) / oldScale,
+            y: (pointer.y - stage.y()) / oldScale,
+        };
+
+        // how to scale? Zoom in? Or zoom out?
+        let direction = e.evt.deltaY > 0 ? 1 : -1;
+
+        // when we zoom on trackpad, e.evt.ctrlKey is true
+        // in that case lets revert direction
+        if (e.evt.ctrlKey) {
+            direction = -direction;
+        }
+
+        const newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+        stage.scale({ x: newScale, y: newScale });
+
+        const newPos = {
+            x: pointer.x - mousePointTo.x * newScale,
+            y: pointer.y - mousePointTo.y * newScale,
+        };
+        stage.position(newPos);
+    })
+
+    stage.on("dragmove", () => {
+        document.body.style.cursor = "grab";
+    })
+
+    stage.on("dragend", () => {
+        document.body.style.cursor = "auto";
+    })
+
+    const layer = window.NodeLayer = new Konva.Layer({name: "Node"});
+    const lineLayer = window.LineLayer = new Konva.Layer({name: "Line"});
+
+    stage.add(lineLayer);
+    stage.add(layer);
+
+    const params = new URLSearchParams(window.location.search)
+    const processId = params.get("process")
+    const version = Number(params.get("v"))
+
+    import("../client/process.js")
+        .then(m => {
+            m.GetPayload(processId, version)
+                .then(process => import("../render_process.js").then(m1 => m1.default(process)))
+        })
+})
+
+function showConnectedServer() {
+    function offline() {
+        const $elem = document.querySelector("[data-type='server-addr']")
+        $elem.classList.remove("success")
+        $elem.classList.remove("error")
+        $elem.classList.add("warning")
+        $elem.textContent = "server offline"
+    }
+
+    function error(msg) {
+        const $elem = document.querySelector("[data-type='server-addr']")
+        $elem.classList.remove("success")
+        $elem.classList.remove("warning")
+        $elem.classList.add("error")
+        $elem.title = msg;
+        $elem.textContent = "server error"
+    }
+
+    function connected(addr) {
+        const $elem = document.querySelector("[data-type='server-addr']")
+        $elem.classList.remove("warning")
+        $elem.classList.remove("error")
+        $elem.classList.add("success")
+        $elem.title = addr;
+        $elem.textContent = "server connected";
+    }
+
+    function check() {
+        const addr = localStorage.getItem("server-addr");
+        if (addr == null || addr === "") {
+            error("url not specified")
+            return
+        }
+
+        fetch(addr+"/health")
+            .then(resp => {
+                if (resp.status !== 200) {
+                    error(resp.statusText);
+                    return
+                }
+
+                connected(addr)
+            })
+            .catch(e => {
+                console.warn(e)
+                offline()
+            })
+    }
+
+    check();
+    setInterval(check, 30000)
+}
+
+function selectServer() {
+    document.getElementById("select-addr-dialog").open = true;
+}
+
+function applyServer() {
+    const $input = document.getElementById("server-addr-input");
+    const url = $input.value;
+    if (url == null || url === "") {
+        $input.classList.add("error")
+        return
+    }
+
+    localStorage.setItem("server-addr", url);
+    document.getElementById("select-addr-dialog").open = false;
+}
