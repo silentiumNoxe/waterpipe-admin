8e436d9d9c49cb9b43afad05d16fc50f64fc8e64
silentiumnoxe <silentium.noxe@gmail.com>
2023-04-16 15:21:05 +0300

    show connected server

--- page/index.js
+++ page/index.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+    showConnectedServer();
     loadData("process")
 })
 
@@ -121,4 +122,73 @@ async function createProcess() {
     client.Save(proc, 1)
         .then(() => closeDialog("create-process"))
         .catch(console.error);
+}
+
+function showConnectedServer() {
+    function offline() {
+        const $elem = document.querySelector("[data-type='server-addr']")
+        $elem.classList.remove("success")
+        $elem.classList.remove("error")
+        $elem.classList.add("warning")
+        $elem.textContent = "server offline"
+    }
+
+    function error(msg) {
+        const $elem = document.querySelector("[data-type='server-addr']")
+        $elem.classList.remove("success")
+        $elem.classList.remove("warning")
+        $elem.classList.add("error")
+        $elem.title = msg;
+        $elem.textContent = "server error"
+    }
+
+    function connected(addr) {
+        const $elem = document.querySelector("[data-type='server-addr']")
+        $elem.classList.remove("warning")
+        $elem.classList.remove("error")
+        $elem.classList.add("success")
+        $elem.title = addr;
+        $elem.textContent = "server connected";
+    }
+
+    function check() {
+        const addr = localStorage.getItem("server-addr");
+        if (addr == null || addr === "") {
+            error("url not specified")
+            return
+        }
+
+        fetch(addr+"/health")
+            .then(resp => {
+                if (resp.status !== 200) {
+                    error(resp.statusText);
+                    return
+                }
+
+                connected(addr)
+            })
+            .catch(e => {
+                console.warn(e)
+                offline()
+            })
+    }
+
+    check();
+    setInterval(check, 30000)
+}
+
+function selectServer() {
+    document.getElementById("select-addr-dialog").open = true;
+}
+
+function applyServer() {
+    const $input = document.getElementById("server-addr-input");
+    const url = $input.value;
+    if (url == null || url === "") {
+        $input.classList.add("error")
+        return
+    }
+
+    localStorage.setItem("server-addr", url);
+    document.getElementById("select-addr-dialog").open = false;
 }
