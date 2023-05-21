57984202f573d17d8fb21d0727fbe35b0091d343
silentiumnoxe <silentium.noxe@gmail.com>
2023-05-21 15:53:15 +0300

    add notify popup

--- util/commons.js
+++ util/commons.js
@@ -1,3 +1,5 @@
+const TIME_SECOND = 1000;
+
 function isNumber(value) {
     if (typeof value !== "string") {
         return false;
@@ -28,3 +30,27 @@ function applyServer() {
     localStorage.setItem("server-addr", url);
     document.getElementById("select-addr-dialog").open = false;
 }
+
+function NotifyPopup(level, message) {
+    const $notify = document.getElementById("notify");
+    if ($notify == null) {
+        console.error("#notify element not found");
+        return
+    }
+
+    const $message = document.createElement("article");
+    $message.textContent = message;
+    $message.classList.add(level);
+
+    $notify.append($message);
+
+    const timeoutId = setTimeout(() => $message.remove(), TIME_SECOND * 15);
+    $message.addEventListener("click", () => {
+        $message.remove();
+        clearTimeout(timeoutId);
+    });
+}
+
+NotifyPopup.SUCCESS = "success";
+NotifyPopup.WARNING = "warning";
+NotifyPopup.ERROR = "error";
