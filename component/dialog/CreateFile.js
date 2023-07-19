a6f6f22b9bfab49ebc79e03a3741e733d078c3ee
silentiumnoxe <silentium.noxe@gmail.com>
2023-07-19 11:14:00 +0300

    add warning message

--- component/dialog/CreateFile.js
+++ component/dialog/CreateFile.js
@@ -31,6 +31,10 @@ const template = `
 </div>
 `;
 
+const warningFolderType = `
+<p><span class="material-symbols-outlined filled warning icon">warning</span>Folder without content will not be saved</p>
+`
+
 customElements.define("waterpipe-create-file", class extends HTMLDialogElement {
 
     static get observedAttributes() {
@@ -54,6 +58,13 @@ customElements.define("waterpipe-create-file", class extends HTMLDialogElement {
 
             setTimeout(() => this.#createFile(data))
         })
+
+        this.querySelector("input[name='type']").addEventListener("change", event => {
+            const value = event.target.value;
+            if (value === "folder") {
+                this.#writeWarning(warningFolderType)
+            }
+        })
     }
 
     connectedCallback() {
@@ -66,6 +77,12 @@ customElements.define("waterpipe-create-file", class extends HTMLDialogElement {
 
     #createFile(data) {
         data.get("type")
+    }
+
+    #writeWarning(template) {
+        const $x = this.querySelector("[data-type='warning']")
+        $x.innerHTML = template
+        $x.classList.remove("hide")
     }
 
 }, {extends: "dialog"})
